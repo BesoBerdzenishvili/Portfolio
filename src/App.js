f9fda7ee13b0ef3da01a06130d746fd7cae64ede
fliderman01 <fliderman01@gmail.com>
2022-11-15 16:00:45 +0400

    added ScrollToTopButton.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from "./layouts/Header";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import ScrollButton from "./components/ScrollButton";
 
 function App() {
   return (
     <div>
       <Header />
       <Skills />
+      <Projects />
+      <ScrollButton />
     </div>
   );
 }
